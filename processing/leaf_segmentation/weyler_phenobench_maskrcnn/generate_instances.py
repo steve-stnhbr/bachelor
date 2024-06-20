26e28e7dc60e1d53dac714d891b4f8c224b4cca2
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-06-20 18:47:07 +0200

    changed options

--- processing/leaf_segmentation/weyler_phenobench_maskrcnn/generate_instances.py
+++ processing/leaf_segmentation/weyler_phenobench_maskrcnn/generate_instances.py
@@ -8,13 +8,13 @@ import click
               "--input_path",
               )
 @click.option("--jpeg", "-j", default=False, is_flag=True)
-def main(path, jpg_images):
-    for file in os.listdir(path + "/images"):
-        if jpg_images:
+def main(input_path, jpeg):
+    for file in os.listdir(input_path + "/images"):
+        if jpeg:
             file = file.replace(".jpg", ".png")
-        img = cv2.imread(path + "/masks/" + file)
-        cv2.imwrite(path + "/plant_instances/" + file, np.where(img != 0, np.ones(img.shape), np.zeros(img.shape)))
-        cv2.imwrite(path + "/leaf_instances/" + file, np.where(img != 0, np.ones(img.shape), np.zeros(img.shape)))
+        img = cv2.imread(input_path + "/masks/" + file)
+        cv2.imwrite(input_path + "/plant_instances/" + file, np.where(img != 0, np.ones(img.shape), np.zeros(img.shape)))
+        cv2.imwrite(input_path + "/leaf_instances/" + file, np.where(img != 0, np.ones(img.shape), np.zeros(img.shape)))
 
 if __name__ == '__main__':
     main()
