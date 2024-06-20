aef1d589fa14135bdfa1a1b3609557ced62d5acc
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-06-20 19:13:04 +0200

    named parameters

--- processing/leaf_segmentation/weyler_phenobench_maskrcnn/generate_instances.py
+++ processing/leaf_segmentation/weyler_phenobench_maskrcnn/generate_instances.py
@@ -5,7 +5,6 @@ import click
 from tqdm import tqdm
 from functools import partial
 import multiprocessing as mp
-from multiprocessing import Pool
 
 @click.command()
 @click.option("-i", 
@@ -13,7 +12,7 @@ from multiprocessing import Pool
               )
 @click.option("--jpeg", "-j", default=False, is_flag=True)
 def main(input_path, jpeg):
-    g = partial(generate, input_path, jpeg)
+    g = partial(generate, input_path=input_path, jpeg=jpeg)
     files = [file for file in os.listdir(os.path.join(input_path, "images")) if ".jpg" in file or ".png" in file]
     print("Generating instances for {} files".format(len(files)))
 
@@ -30,7 +29,6 @@ def generate(input_path, jpeg, file):
     img = cv2.imread(input_path + "/masks/" + file)
     cv2.imwrite(input_path + "/plant_instances/" + file, np.where(img != 0, np.ones(img.shape), np.zeros(img.shape)))
     cv2.imwrite(input_path + "/leaf_instances/" + file, np.where(img != 0, np.ones(img.shape), np.zeros(img.shape)))
-
 
 if __name__ == '__main__':
     main()
