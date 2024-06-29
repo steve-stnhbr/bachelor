8e85c28f5c20d86c1d03b6e3b0ba5c6298979ea5
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-06-29 15:47:04 +0200

    sample

---
+++ processing/disease_detection/_data/sample_files.py
@@ -0,0 +1,30 @@
+import os
+import click
+import random
+import shutil
+
+@click.command()
+@click.option("-i", "--input")
+@click.option('-o', '--output')
+@click.option('-s', '--split', default=None)
+@click.option('-r', '--ratio')
+def main(input, output, split, ratio):
+    
+    files = []
+    for root, dirs, files in os.walk(input):
+        files.extend([os.path.relpath(os.path.join(root, file), input) for file in files])
+
+    sampled = random.sample(files, (int(len(files) * ratio)))
+
+    if split is not None:
+        files = list(set(files).difference(set(sampled)))
+        for file in files:
+            shutil.copy(os.path.join(input, file), os.path.join(split, file))
+
+    for file in sampled:
+        shutil.copy(os.path.join(input, file), os.path.join(output, file))
+    
+    
+
+if __name__ == '__main__':
+    main()
