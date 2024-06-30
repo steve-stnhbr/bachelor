655a1614cb3a9ee1540fe8dea81ced13d4952b6f
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-06-30 11:21:19 +0200

    changed determination of length

--- processing/disease_detection/schuler_two_branch/k/cai/datasets.py
+++ processing/disease_detection/schuler_two_branch/k/cai/datasets.py
@@ -1007,7 +1007,7 @@ def load_images_from_files(file_names, target_size=(224,224), dtype='float32', s
       except UnidentifiedImageError:
         print("Error reading image, will pad this with existing data")
     x.extend(x)
-    return np.array(x[:cnt], dtype=dtype)
+    return np.array(x[:len(file_names)], dtype=dtype)
 
 def load_images_from_folders(seed=None, root_dir=None, lab=False, 
   verbose=True, bipolar=False, base_model_name='plant_leaf',
