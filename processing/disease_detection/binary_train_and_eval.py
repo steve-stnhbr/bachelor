43aa5885a961a6bd0f40e1d07eb703b19fb5ee1c
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-01 18:30:14 +0200

    master trainer

---
+++ processing/disease_detection/binary_train_and_eval.py
@@ -0,0 +1,81 @@
+import cai.datasets
+from schuler_two_branch.test_datagen import PlantLeafsDataGen
+import cai
+import os
+from tensorflow import keras
+import keras.appications
+
+L_RATIO = .8
+TWO_PATHS_SECOND_BLOCK = True
+INPUT_SHAPE = (224, 224, 3)
+CLASSES = 2
+
+TRAIN_DATA_PATH = os.path.join("_data", "train")
+VAL_DATA_PATH = os.path.join("_data", "val")
+TEST_DATA_PATH = os.path.join("_data", "test")
+
+def load_transform(paths):
+    return cai.datasets.load_images_from_files(paths, target_size=INPUT_SHAPE[:2], lab=True, rescale=True, smart_resize=True)
+
+def execute(model, name=None, lab=False, batch_size=32, workers=16):
+    if name is None:
+        name = type(model).__name__
+    print(f"Starting training for {name}")
+
+    opt = keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer=opt,
+        metrics=['accuracy']
+    )
+    
+    print("Creating datagen")
+    train_datagen = PlantLeafsDataGen(TRAIN_DATA_PATH, transforms=[load_transform] if lab else None, batch_size=batch_size, workers=workers, use_multiprocessing=True)
+    val_datagen = PlantLeafsDataGen(VAL_DATA_PATH, transforms=[load_transform] if lab else None, batch_size=batch_size, workers=workers, use_multiprocessing=True)
+    test_datagen = PlantLeafsDataGen(TEST_DATA_PATH, transforms=[load_transform] if lab else None, batch_size=batch_size, workers=workers, use_multiprocessing=True)
+
+    callbacks = [
+        keras.callbacks.EarlyStopping(patience=2),
+        keras.callbacks.ModelCheckpoint(filepath='checkpoints/model##name##.{epoch:02d}.keras'.replace("##name##", name)),
+        keras.callbacks.TensorBoard(log_dir='./logs'),
+        keras.callbacks.ModelCheckpoint(filepath='out/best##name##.keras'.replace('##name##', name), save_best_only=True, mode='max', monitor='val_accuracy')
+    ]
+    print(f"Beginning training of model {name}")
+
+    model.fit(train_datagen, epochs=15, callbacks=callbacks, validation_data=val_datagen)
+
+    print("Training finished, starting test evaluation")
+
+    result = model.eval(test_datagen)
+    print(result)
+
+def main():
+    models = [
+        keras.applications.ResNet152V2(
+            include_top = True,
+            input_shape=INPUT_SHAPE,
+            classes=CLASSES
+        ),
+        keras.applications.InceptionV3(
+            include_top=True,
+            input_shape=INPUT_SHAPE,
+            classes=CLASSES
+        ),
+        keras.applications.MobileNetV3Large(
+            include_top=True,
+            input_shape=INPUT_SHAPE,
+            classes=CLASSES
+        ),
+        keras.applications.ConvNeXtLarge(
+            include_top=True,
+            input_shape=INPUT_SHAPE,
+            classes=CLASSES
+        )
+    ]
+
+    for model in models:
+        for lab in [True, False]:
+            execute(model, f"{ type(model).__name__}_{'lab' if lab else 'rgb'}", lab)
+
+if __name__ == '__main__':
+    main()
