3954557c35cbeb2d12bdbcd7b6b3b0044ebe28f1
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-01 18:45:20 +0200

    dircheck

--- processing/disease_detection/schuler_two_branch/test_datagen.py
+++ processing/disease_detection/schuler_two_branch/test_datagen.py
@@ -50,7 +50,7 @@ class PlantLeafsDataGenBinary(PyDataset):
         self.shuffle = shuffle
         self.on_epoch_end()
 
-        self.classes = os.listdir(path)
+        self.classes = [dir for dir in os.listdir(path) if os.path.isdir(dir)]
         self.num_classes = 2
 
         self.file_paths = []
