bed32e4ceb8aae03e541ae4dc8c10ebfb0ff09fe
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-02 10:51:45 +0200

    redid stuff

--- processing/disease_detection/binary_train_and_eval.py
+++ processing/disease_detection/binary_train_and_eval.py
@@ -8,6 +8,9 @@ import click
 import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
 import numpy as np
+import cv2 
+import skimage_color
+from functools import partial
 
 L_RATIO = .8
 TWO_PATHS_SECOND_BLOCK = True
@@ -39,9 +42,9 @@ def execute(model, name=None, lab=False, batch_size=32, workers=16):
     # val_datagen = PlantLeafsDataGenBinary(VAL_DATA_PATH, transforms=[load_transform] if lab else None, batch_size=batch_size, workers=workers, use_multiprocessing=True)
     # test_datagen = PlantLeafsDataGenBinary(TEST_DATA_PATH, transforms=[load_transform] if lab else None, batch_size=batch_size, workers=workers, use_multiprocessing=True)
 
-    train_datagen = gen_dataset(TRAIN_DATA_PATH, batch_size=batch_size)
-    val_datagen = gen_dataset(VAL_DATA_PATH, batch_size=batch_size)
-    test_datagen = gen_dataset(TEST_DATA_PATH, batch_size=batch_size)
+    train_datagen = gen_dataset(TRAIN_DATA_PATH, batch_size=batch_size, lab=lab)
+    val_datagen = gen_dataset(VAL_DATA_PATH, batch_size=batch_size, lab=lab)
+    test_datagen = gen_dataset(TEST_DATA_PATH, batch_size=batch_size, lab=lab)
 
     test = train_datagen.take(5).as_numpy_iterator()
     for el in test:
@@ -64,10 +67,12 @@ def execute(model, name=None, lab=False, batch_size=32, workers=16):
     result = model.evaluate(test_datagen)
     print(result)
 
-def gen_dataset(path, batch_size):
+def gen_dataset(path, batch_size, lab):
     def map_data(x, y):
         return (x, to_categorical(y, num_classes=2))
     datagen = keras.utils.image_dataset_from_directory(path, batch_size=batch_size, image_size=INPUT_SHAPE[:2], crop_to_aspect_ratio=True, labels="inferred", label_mode="binary")
+    if lab:
+        datagen = datagen.map(partial(transform, smart_resize=True, lab=True, rescale=True), num_parallel_calls=tf.data.AUTOTUNE, deterministic=False)
     datagen = datagen.map(map_data, num_parallel_calls=tf.data.AUTOTUNE, deterministic=False).prefetch(tf.data.AUTOTUNE)
     return datagen
 
@@ -121,6 +126,54 @@ def main(workers, batch_size):
     for model, name in models:
         for lab in [True, False]:
             execute(model, f"{name}_{'lab' if lab else 'rgb'}", lab, workers=workers, batch_size=batch_size)
+
+
+def transform(img, target_size=(224,224), smart_resize=False, lab=False, rescale=False, bipolar=False):
+    def local_rescale(img,  lab):
+        if (lab):
+            # JP prefers bipolar input [-2,+2]
+            if (bipolar):
+                img[:,:,0:3] /= [25, 50, 50]
+                img[:,:,0] -= 2
+            else:
+                img[:,:,0:3] /= [100, 200, 200]
+                img[:,:,1:3] += 0.5
+        else:
+            if (bipolar):
+                img /= 64
+                img -= 2
+            else:
+                img /= 255
+    def add_padding_to_make_img_array_squared(img):
+        """ Adds padding to make the image squared.
+        # Arguments
+            img: an image as an array.
+        """
+        sizex = img.shape[0]
+        sizey = img.shape[1]
+        if (sizex == sizey):
+            return img
+        else:
+            maxsize = np.max([sizex, sizey])
+            padx = (maxsize - sizex) // 2
+            pady = (maxsize - sizey) // 2
+            return np.pad(img, pad_width=((padx,padx),(pady,pady),(0,0)))
+    if (smart_resize):
+        if (lab):
+            img /= 255
+            img = skimage_color.rgb2lab(img)
+        if(rescale):
+            local_rescale(img,  lab)
+        img = add_padding_to_make_img_array_squared(img)
+        if ((img.shape[0] != target_size[0]) or (img.shape[1] != target_size[1])):
+            img = cv2.resize(img, dsize=target_size, interpolation=cv2.INTER_NEAREST)
+    else:
+        if (lab):
+            img /= 255
+            img = skimage_color.rgb2lab(img)
+        if(rescale):
+            local_rescale(img,  lab)
+    return img
 
 if __name__ == '__main__':
     main()
