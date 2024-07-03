84e3f97a1b0d9432d6cf5d743a5889c0d336654f
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-03 09:24:16 +0200

    pts

--- processing/disease_detection/binary_train_and_eval.py
+++ processing/disease_detection/binary_train_and_eval.py
@@ -187,13 +187,26 @@ def transform(img, target_size=(224,224), smart_resize=False, lab=False, rescale
             pady = (maxsize - sizey) // 2
             return np.pad(img, pad_width=((padx, maxsize - sizex - padx), (pady, maxsize - sizey - pady), (0, 0)))
             #return tf.pad(img, [[padx, padx], [pady, pady]])
+    
+    def pad_to_square(img):
+        # Get the current size of the image
+        shape = tf.shape(img)
+        height, width = shape[0], shape[1]
+
+        # Determine the size of the new square image
+        max_dim = tf.maximum(height, width)
+
+        # Pad the image to make it square
+        squared_img = tf.image.resize_with_crop_or_pad(img, target_height=max_dim, target_width=max_dim)
+
+        return squared_img
     if (smart_resize):
         if (lab):
             img /= 255
             img = skimage_color.rgb2lab(img)
         if(rescale):
             local_rescale(img,  lab)
-        img = add_padding_to_make_img_array_squared(img)
+        img = pad_to_square(img)
         if ((img.shape[0] != target_size[0]) or (img.shape[1] != target_size[1])):
             img = cv2.resize(img, dsize=target_size, interpolation=cv2.INTER_NEAREST)
     else:
