715a64b298c3ff703fdd0968ff2a4ba434c8d9a7
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-03 10:01:46 +0200

    bb

--- processing/disease_detection/binary_train_and_eval.py
+++ processing/disease_detection/binary_train_and_eval.py
@@ -209,7 +209,7 @@ def transform(img, target_size=(224,224), smart_resize=False, lab=False, rescale
             local_rescale(img,  lab)
         img = pad_to_square(img)
         if ((img.shape[0] != target_size[0]) or (img.shape[1] != target_size[1])):
-            print("TARGET_SIZE_2=", target_size, type(target_size))
+            print("TARGET_SIZE_2=", target_size, type(target_size), (not target_size.empty()))
             img = cv2.resize(img, target_size, interpolation=cv2.INTER_NEAREST)
     else:
         if (lab):
