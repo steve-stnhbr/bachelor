be5754c4de3c032e306dfa76ec17756ab2512f20
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-03 13:26:10 +0200

    added layer output util

---
+++ processing/disease_detection/layer_output_vis.py
@@ -0,0 +1,55 @@
+import os
+import click
+import keras
+from keras import backend as K
+import cv2
+from custom_utils import transform
+
+@click.command()
+@click.option('-m', '--model')
+@click.option('-i', '--input')
+@click.option('-o', '--output')
+@click.option('-l', '--lab', is_flag=True)
+def main(model, input, output, lab):
+    if not os.path.exists(input):
+        raise FileNotFoundError(input)
+    
+    if ',' in model:
+        models = model.split(',')
+        for model in models:
+            handle_model(model, input, output, lab)
+    else:
+        handle_model(model, input, output, lab)
+
+def handle_model(model, input, output, lab):
+    model = keras.load_model(model)
+
+    if os.path.isdir(input):
+        for file in os.listdir(input):
+            visualize(model, os.path.join(input, file), output, lab)
+    else:
+        visualize(model, input, output, lab)
+
+def visualize(model, file, output, lab=False):
+    # create output dir
+    folder = os.path.join(output, model, file[:file.index('.')])
+    os.makedirs(folder, exist_ok=True)
+
+    # read input
+    img = cv2.imread(input)
+    img = transform(img, lab=lab, rescale=True, smart_resize=True)
+
+    # create output function
+    inp = model.input                                           # input placeholder
+    outputs = [layer.output for layer in model.layers]          # all layer outputs
+    functor = K.function([inp, K.learning_phase()], outputs )   # evaluation function
+
+    # calculating outputs
+    layer_outs = functor([img, 1.])
+
+    for name, output in layer_outs:
+        print(f"Writing output for layer {name} of image {file}")
+        cv2.imwrite(os.path.join(folder, name + ".png"), output)
+
+if __name__ == '__main__':
+    main()
