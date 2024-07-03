68994803f7f6fae886d8cd448c7b248464cc0210
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-03 10:10:37 +0200

    stack

--- processing/disease_detection/binary_train_and_eval.py
+++ processing/disease_detection/binary_train_and_eval.py
@@ -155,80 +155,81 @@ def main(workers, batch_size):
             execute(model, f"{name}_{'lab' if lab else 'rgb'}", lab, workers=workers, batch_size=batch_size)
 
 
-def transform(img, target_size=(224,224), smart_resize=False, lab=False, rescale=False, bipolar=False):
-    img = img.numpy().astype(np.float32)
-    print("TARGET_SIZE=", target_size)
-    def local_rescale(img,  lab):
-        if (lab):
-            # JP prefers bipolar input [-2,+2]
-            if (bipolar):
-                img[:,:,0:3] /= [25, 50, 50]
-                img[:,:,0] -= 2.
-            else:
-                img[:,:,0:3] /= [100, 200, 200]
-                img[:,:,1:3] += 0.5
-        else:
-            if (bipolar):
-                img /= 64.
-                img -= 2.
-            else:
-                img /= 255.
-    def add_padding_to_make_img_array_squared(img):
-        """ Adds padding to make the image squared.
-        # Arguments
-            img: an image as an array.
-        """
-        sizex = img.shape[0]
-        sizey = img.shape[1]
-        if (sizex == sizey):
-            return img
-        else:
-            maxsize = np.max([sizex, sizey])
-            padx = (maxsize - sizex) // 2
-            pady = (maxsize - sizey) // 2
-            return np.pad(img, pad_width=((padx, maxsize - sizex - padx), (pady, maxsize - sizey - pady), (0, 0)))
-            #return tf.pad(img, [[padx, padx], [pady, pady]])
-    
-    def pad_to_square(img):
-        # Get the current size of the image
-        shape = tf.shape(img)
-        height, width = shape[0], shape[1]
-
-        # Determine the size of the new square image
-        max_dim = tf.maximum(height, width)
-
-        # Pad the image to make it square
-        squared_img = tf.image.resize_with_crop_or_pad(img, target_height=max_dim, target_width=max_dim)
-
-        return squared_img.numpy()
-    if (smart_resize):
-        if (lab):
-            img /= 255
-            img = skimage_color.rgb2lab(img)
-        if(rescale):
-            local_rescale(img,  lab)
-        img = pad_to_square(img)
-        if ((img.shape[0] != target_size[0]) or (img.shape[1] != target_size[1])):
-            print("TARGET_SIZE_2=", target_size, type(target_size))
-            print("IMG_SIZE=", img.shape)
-            img = cv2.resize(img, target_size, interpolation=cv2.INTER_NEAREST)
-    else:
-        if (lab):
-            img /= 255.
-            img = skimage_color.rgb2lab(img)
-        if(rescale):
-            local_rescale(img,  lab)
-    return tf.convert_to_tensor(img, dtype=tf.float32)
-
-def transform_wrapper(imgs, target_size=(224,224), smart_resize=False, lab=False, rescale=False, bipolar=False):
-    out = np.ndarray()
+def transform(imgs, target_size=(224,224), smart_resize=False, lab=False, rescale=False, bipolar=False):
+    arr = []
     for i in range(imgs.shape()[0]):
         img = imgs[i]
-        p = partial(transform, target_size=target_size, smart_resize=smart_resize, lab=lab, rescale=rescale, bipolar=bipolar)
-        img = tf.py_function(func=p, inp=[img], Tout=tf.float32)
-        img.set_shape(target_size + (3,))
-        out[i] = img
-    return out
+        img = img.numpy().astype(np.float32)
+        print("TARGET_SIZE=", target_size)
+        def local_rescale(img,  lab):
+            if (lab):
+                # JP prefers bipolar input [-2,+2]
+                if (bipolar):
+                    img[:,:,0:3] /= [25, 50, 50]
+                    img[:,:,0] -= 2.
+                else:
+                    img[:,:,0:3] /= [100, 200, 200]
+                    img[:,:,1:3] += 0.5
+            else:
+                if (bipolar):
+                    img /= 64.
+                    img -= 2.
+                else:
+                    img /= 255.
+        def add_padding_to_make_img_array_squared(img):
+            """ Adds padding to make the image squared.
+            # Arguments
+                img: an image as an array.
+            """
+            sizex = img.shape[0]
+            sizey = img.shape[1]
+            if (sizex == sizey):
+                return img
+            else:
+                maxsize = np.max([sizex, sizey])
+                padx = (maxsize - sizex) // 2
+                pady = (maxsize - sizey) // 2
+                return np.pad(img, pad_width=((padx, maxsize - sizex - padx), (pady, maxsize - sizey - pady), (0, 0)))
+                #return tf.pad(img, [[padx, padx], [pady, pady]])
+        
+        def pad_to_square(img):
+            # Get the current size of the image
+            shape = tf.shape(img)
+            height, width = shape[0], shape[1]
+
+            # Determine the size of the new square image
+            max_dim = tf.maximum(height, width)
+
+            # Pad the image to make it square
+            squared_img = tf.image.resize_with_crop_or_pad(img, target_height=max_dim, target_width=max_dim)
+
+            return squared_img.numpy()
+        if (smart_resize):
+            if (lab):
+                img /= 255
+                img = skimage_color.rgb2lab(img)
+            if(rescale):
+                local_rescale(img,  lab)
+            img = pad_to_square(img)
+            if ((img.shape[0] != target_size[0]) or (img.shape[1] != target_size[1])):
+                print("TARGET_SIZE_2=", target_size, type(target_size))
+                print("IMG_SIZE=", img.shape)
+                img = cv2.resize(img, target_size, interpolation=cv2.INTER_NEAREST)
+        else:
+            if (lab):
+                img /= 255.
+                img = skimage_color.rgb2lab(img)
+            if(rescale):
+                local_rescale(img,  lab)
+        arr.append(img)
+    return tf.convert_to_tensor(np.stack(arr), dtype=tf.float32)
+
+def transform_wrapper(imgs, target_size=(224,224), smart_resize=False, lab=False, rescale=False, bipolar=False):
+    size = imgs.shape()[0]
+    p = partial(transform, target_size=target_size, smart_resize=smart_resize, lab=lab, rescale=rescale, bipolar=bipolar)
+    imgs = tf.py_function(func=p, inp=[imgs], Tout=tf.float32)
+    imgs.set_shape((size,) + target_size + (3,))
+    return imgs
 
 if __name__ == '__main__':
     main()
