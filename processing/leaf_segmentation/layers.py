9fa9c19d7d8d5207d0c236c2c9640e8130c07209
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-06 15:19:41 +0200

    plap

--- processing/leaf_segmentation/layers.py
+++ processing/leaf_segmentation/layers.py
@@ -1,116 +1,9 @@
-#title           :pooling.py
-#description     :implementation of max_pooling_with_argmax and unpooling for tensorflow and keras
-#author          :yselivonchyk
-#date            :20190405
-#modeldetails    :non-sequential model, parallel training as a multiple output model
-
-import tensorflow as tf
-import tensorflow.keras as keras
-import tensorflow.keras.backend as K
-from tensorflow.keras.layers import Layer, Lambda
-
-
-# Tensorflow implementations of max_pooling and unpooling
-
-
-def max_pool_with_argmax(net, ksize, strides):
-  assert isinstance(ksize, list) or isinstance(ksize, int)
-  assert isinstance(strides, list) or isinstance(strides, int)
-
-  ksize = ksize if isinstance(ksize, list) else [1, ksize, ksize, 1]
-  strides = strides if isinstance(strides, list) else [1, strides, strides, 1]
-
-  with tf.name_scope('MaxPoolArgMax'):
-    net, mask = tf.nn.max_pool_with_argmax(
-      net,
-      ksize=ksize,
-      strides=strides,
-      padding='SAME')
-    return net, mask
-
-
-def unpool(pool, ind, ksize=[1, 2, 2, 1], scope='unpool'):
-    """
-       Unpooling layer after max_pool_with_argmax.
-       Args:
-           pool:   max pooled output tensor
-           ind:      argmax indices
-           ksize:     ksize is the same as for the pool
-       Return:
-           unpool:    unpooling tensor
-    """
-    input_shape = tf.shape(pool)
-    output_shape = [input_shape[0], input_shape[1] * ksize[1], input_shape[2] * ksize[2], input_shape[3]]
-
-    flat_input_size = tf.reduce_prod(input_shape)
-    flat_output_shape = [output_shape[0], output_shape[1] * output_shape[2] * output_shape[3]]
-
-    pool_ = tf.reshape(pool, [flat_input_size])
-    batch_range = tf.reshape(tf.range(tf.cast(output_shape[0], tf.int64), dtype=ind.dtype),
-                                        shape=[input_shape[0], 1, 1, 1])
-    b = tf.ones_like(ind) * batch_range
-    b1 = tf.reshape(b, [flat_input_size, 1])
-    ind_ = tf.reshape(ind, [flat_input_size, 1])
-    ind_ = tf.concat([b1, ind_], 1)
-
-    ret = tf.scatter_nd(ind_, pool_, shape=tf.cast(flat_output_shape, tf.int64))
-    ret = tf.reshape(ret, output_shape)
-
-    set_input_shape = pool.get_shape()
-    set_output_shape = [set_input_shape[0], set_input_shape[1] * ksize[1], set_input_shape[2] * ksize[2], set_input_shape[3]]
-    ret.set_shape(set_output_shape)
-    return ret
-
-
-def _upsample_along_axis(volume, axis, stride, mode='ZEROS'):
-  shape = volume.get_shape().as_list()
-
-  assert mode in ['COPY', 'ZEROS']
-  assert 0 <= axis < len(shape)
-
-  target_shape = shape[:]
-  target_shape[axis] *= stride
-
-  padding = tf.zeros(shape, dtype=volume.dtype) if mode == 'ZEROS' else volume
-  parts = [volume] + [padding for _ in range(stride - 1)]
-  volume = tf.concat(parts, min(axis+1, len(shape)-1))
-
-  volume = tf.reshape(volume, target_shape)
-  return volume
-
-
-def upsample(net, stride, mode='ZEROS'):
-  """
-  Imitate reverse operation of Max-Pooling by either placing original max values
-  into a fixed postion of upsampled cell:
-  [0.9] =>[[.9, 0],   (stride=2)
-           [ 0, 0]]
-  or copying the value into each cell:
-  [0.9] =>[[.9, .9],  (stride=2)
-           [ .9, .9]]
-
-  :param net: 4D input tensor with [batch_size, width, heights, channels] axis
-  :param stride:
-  :param mode: string 'ZEROS' or 'COPY' indicating which value to use for undefined cells
-  :return:  4D tensor of size [batch_size, width*stride, heights*stride, channels]
-  """
-  assert mode in ['COPY', 'ZEROS']
-  with tf.name_scope('Upsampling'):
-    net = _upsample_along_axis(net, 2, stride, mode=mode)
-    net = _upsample_along_axis(net, 1, stride, mode=mode)
-    return net
-
-
-# Keras layers for pooling and unpooling
+from keras import backend as K
+from keras.layers import Layer
 
 
 class MaxPoolingWithArgmax2D(Layer):
-    def __init__(
-            self,
-            pool_size=(2, 2),
-            strides=(2, 2),
-            padding='same',
-            **kwargs):
+    def __init__(self, pool_size=(2, 2), strides=(2, 2), padding="same", **kwargs):
         super(MaxPoolingWithArgmax2D, self).__init__(**kwargs)
         self.padding = padding
         self.pool_size = pool_size
@@ -120,13 +13,17 @@ class MaxPoolingWithArgmax2D(Layer):
         padding = self.padding
         pool_size = self.pool_size
         strides = self.strides
-        if K.backend() == 'tensorflow':
+        if K.backend() == "tensorflow":
             ksize = [1, pool_size[0], pool_size[1], 1]
+            padding = padding.upper()
             strides = [1, strides[0], strides[1], 1]
-            output, argmax = Lambda(lambda x: max_pool_with_argmax(x, ksize, strides))(inputs)
+            output, argmax = K.tf.nn.max_pool_with_argmax(
+                inputs, ksize=ksize, strides=strides, padding=padding
+            )
         else:
-            errmsg = '{} backend is not supported for layer {}'.format(
-                K.backend(), type(self).__name__)
+            errmsg = "{} backend is not supported for layer {}".format(
+                K.backend(), type(self).__name__
+            )
             raise NotImplementedError(errmsg)
         argmax = K.cast(argmax, K.floatx())
         return [output, argmax]
@@ -134,9 +31,9 @@ class MaxPoolingWithArgmax2D(Layer):
     def compute_output_shape(self, input_shape):
         ratio = (1, 2, 2, 1)
         output_shape = [
-            dim // ratio[idx]
-            if dim is not None else None
-            for idx, dim in enumerate(input_shape)]
+            dim // ratio[idx] if dim is not None else None
+            for idx, dim in enumerate(input_shape)
+        ]
         output_shape = tuple(output_shape)
         return [output_shape, output_shape]
 
@@ -145,39 +42,49 @@ class MaxPoolingWithArgmax2D(Layer):
 
 
 class MaxUnpooling2D(Layer):
-    def __init__(self, pool_size=(2, 2), **kwargs):
+    def __init__(self, size=(2, 2), **kwargs):
         super(MaxUnpooling2D, self).__init__(**kwargs)
-        self.pool_size = pool_size
+        self.size = size
 
     def call(self, inputs, output_shape=None):
-        updates = inputs[0]
-        mask    = tf.cast(inputs[1], dtype=tf.int64)
-        ksize   = [1, self.pool_size[0], self.pool_size[1], 1]
-        return unpool(updates, mask, ksize)
+        updates, mask = inputs[0], inputs[1]
+        with K.tf.variable_scope(self.name):
+            mask = K.cast(mask, "int32")
+            input_shape = K.tf.shape(updates, out_type="int32")
+            #  calculation new shape
+            if output_shape is None:
+                output_shape = (
+                    input_shape[0],
+                    input_shape[1] * self.size[0],
+                    input_shape[2] * self.size[1],
+                    input_shape[3],
+                )
+            self.output_shape1 = output_shape
+
+            # calculation indices for batch, height, width and feature maps
+            one_like_mask = K.ones_like(mask, dtype="int32")
+            batch_shape = K.concatenate([[input_shape[0]], [1], [1], [1]], axis=0)
+            batch_range = K.reshape(
+                K.tf.range(output_shape[0], dtype="int32"), shape=batch_shape
+            )
+            b = one_like_mask * batch_range
+            y = mask // (output_shape[2] * output_shape[3])
+            x = (mask // output_shape[3]) % output_shape[2]
+            feature_range = K.tf.range(output_shape[3], dtype="int32")
+            f = one_like_mask * feature_range
+
+            # transpose indices & reshape update values to one dimension
+            updates_size = K.tf.size(updates)
+            indices = K.transpose(K.reshape(K.stack([b, y, x, f]), [4, updates_size]))
+            values = K.reshape(updates, [updates_size])
+            ret = K.tf.scatter_nd(indices, values, output_shape)
+            return ret
 
     def compute_output_shape(self, input_shape):
         mask_shape = input_shape[1]
         return (
             mask_shape[0],
-            mask_shape[1] * self.pool_size[0],
-            mask_shape[2] * self.pool_size[1],
-            mask_shape[3]
+            mask_shape[1] * self.size[0],
+            mask_shape[2] * self.size[1],
+            mask_shape[3],
         )
-
-
-# Test implementation
-
-
-if __name__ == '__main__':
-    import numpy as np
-
-    input = keras.layers.Input((4, 4, 3))
-    (e, m) = MaxPoolingWithArgmax2D()(input)
-    o2 = MaxUnpooling2D()([e, m])
-    model = keras.Model(inputs=input, outputs=o2)  # outputs=o
-    model.compile(optimizer="adam", loss='categorical_crossentropy')
-    model.summary()
-    x = np.random.randint(0, 100, (3, 4, 4, 3))
-    m = model.predict(x)
-    print(x[0])
-    print(m[0])
