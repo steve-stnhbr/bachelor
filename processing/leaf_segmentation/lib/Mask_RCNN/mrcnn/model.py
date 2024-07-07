e4b4a466fb2dcbfe3557a24e49a470950308783b
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-07 19:56:24 +0200

    ll

--- processing/leaf_segmentation/lib/Mask_RCNN/mrcnn/model.py
+++ processing/leaf_segmentation/lib/Mask_RCNN/mrcnn/model.py
@@ -1876,7 +1876,7 @@ class MaskRCNN():
                 shape=[None, 4], name="input_gt_boxes", dtype=tf.float32)
             # Normalize coordinates
             gt_boxes = KL.Lambda(lambda x: norm_boxes_graph(
-                x, K.shape(input_image)[1:3]))(input_gt_boxes)
+                x, K.shape(input_image)[1:3]), output_shape=(None, 4))(input_gt_boxes)
             # 3. GT Masks (zero padded)
             # [batch, height, width, MAX_GT_INSTANCES]
             if config.USE_MINI_MASK:
