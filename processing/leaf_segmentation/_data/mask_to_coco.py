b97f9bb756195b06a42fc2c76ce269347b8a126f
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-08 11:51:36 +0200

    mp

--- processing/leaf_segmentation/_data/mask_to_coco.py
+++ processing/leaf_segmentation/_data/mask_to_coco.py
@@ -7,6 +7,7 @@ import os
 import glob
 import click
 import tqdm
+from multiprocessing import Pool, cpu_count
 
 def create_coco_annotation(mask, image_id, category_id, annotation_id):
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -30,6 +31,36 @@ def create_coco_annotation(mask, image_id, category_id, annotation_id):
     }
     return annotation
 
+def process_single_image(args):
+    img_path, mask_path, image_id, annotation_id_start = args
+    mask_image = Image.open(mask_path)
+    mask = np.array(mask_image)
+    
+    unique_categories = np.unique(mask)
+    unique_categories = unique_categories[unique_categories > 0]  # Exclude background (assumed to be 0)
+    
+    annotations = []
+    height, width = mask.shape
+    annotation_id = annotation_id_start
+    
+    for category_id in unique_categories:
+        category_mask = (mask == category_id).astype(np.uint8)
+        if category_mask.sum() == 0:
+            continue
+        
+        annotation = create_coco_annotation(category_mask, image_id, int(category_id), annotation_id)
+        annotations.append(annotation)
+        annotation_id += 1
+    
+    image_info = {
+        "id": image_id,
+        "width": width,
+        "height": height,
+        "file_name": os.path.basename(img_path)
+    }
+    
+    return image_info, annotations, list(unique_categories)
+
 def convert_masks_to_coco(image_dir, mask_dir, output_path):
     image_paths = sorted(glob.glob(os.path.join(image_dir, "*.png")))
     mask_paths = sorted(glob.glob(os.path.join(mask_dir, "*.png")))
@@ -37,39 +68,19 @@ def convert_masks_to_coco(image_dir, mask_dir, output_path):
     images = []
     annotations = []
     categories = set()
-    annotation_id = 1
-    image_id = 1
     
-    p_bar = tqdm.tqdm(zip(image_paths, mask_paths), total=len(image_paths))
-
-    for img_path, mask_path in p_bar:
-        p_bar.set_description_str(f"Processing {os.path.basename(img_path)}")
-        mask_image = Image.open(mask_path)
-        mask = np.array(mask_image)
+    with Pool(cpu_count()) as pool:
+        image_id = 1
+        annotation_id_start = 1
+        args = [(img_path, mask_path, image_id + idx, annotation_id_start + idx * 1000) 
+                for idx, (img_path, mask_path) in enumerate(zip(image_paths, mask_paths))]
         
-        unique_categories = np.unique(mask)
-        unique_categories = unique_categories[unique_categories > 0]  # Exclude background (assumed to be 0)
+        results = list(tqdm.tqdm(pool.map(process_single_image, args), total = len(image_paths)))
         
-        height, width = mask.shape
-        
-        images.append({
-            "id": image_id,
-            "width": width,
-            "height": height,
-            "file_name": os.path.basename(img_path)
-        })
-        
-        for category_id in unique_categories:
-            categories.add(int(category_id))
-            category_mask = (mask == category_id).astype(np.uint8)
-            if category_mask.sum() == 0:
-                continue
-            
-            annotation = create_coco_annotation(category_mask, image_id, int(category_id), annotation_id)
-            annotations.append(annotation)
-            annotation_id += 1
-        
-        image_id += 1
+        for image_info, annots, cats in results:
+            images.append(image_info)
+            annotations.extend(annots)
+            categories.update(cats)
     
     categories = [{"id": int(category_id), "name": f"category_{category_id}"} for category_id in sorted(categories)]
     
