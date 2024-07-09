81f284768a21b6044d42cf9452f1786c6d9f722e
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-09 11:43:44 +0200

    sq

--- processing/leaf_segmentation/train_and_eval.py
+++ processing/leaf_segmentation/train_and_eval.py
@@ -97,7 +97,7 @@ def gen_dataset(path, mask_subdir, batch_size, lab):
         datagen = datagen.map(
             lambda x, y: (transform_wrapper(x, target_size=INPUT_SHAPE[:2], rescale=True, smart_resize=True, lab=True), y)
         , num_parallel_calls=tf.data.AUTOTUNE, deterministic=False)
-    datagen = datagen.prefetch(tf.data.AUTOTUNE).batch(batch_size=batch_size, drop_remainder=True)
+    datagen = datagen.map(lambda x,y: (tf.squeeze(x), tf.squeeze(y))).batch(batch_size=batch_size, drop_remainder=True).prefetch(tf.data.AUTOTUNE)
     for s in datagen.take(1).as_numpy_iterator():
         print("X", s[0].shape, tf.reduce_max(s[0]).numpy())
         print("Y", s[1].shape, tf.reduce_max(s[1]).numpy())
