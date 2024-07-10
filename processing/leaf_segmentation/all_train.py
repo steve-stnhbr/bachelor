c0ce9157021027189cce9b3c62636943747547fa
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-10 13:42:05 +0200

    ll

--- processing/leaf_segmentation/all_train.py
+++ processing/leaf_segmentation/all_train.py
@@ -9,6 +9,8 @@ from keras.callbacks import ModelCheckpoint
 import tensorflow as tf
 import glob
 import sys
+import keras
+import click
 
 def find_latest_checkpoint(checkpoints_path, fail_safe=True):
 
@@ -206,3 +208,37 @@ def train(model,
                   validation_steps=val_steps_per_epoch,
                   epochs=epochs, callbacks=callbacks,
                   use_multiprocessing=gen_use_multiprocessing, initial_epoch=initial_epoch)
+        
+
+@click.command()
+@click.option('-m', '--model', type=str)
+@click.option('-i', '--input', type=str)
+@click.option('-a', '--augment', is_flag=True)
+@click.option('-n', '--no-validate', is_flag=True)
+@click.option('-c', '--classes', default=25)
+def main(model, input, augment, no_validate, classes):
+    train_images = os.path.join(input, "train", "images")
+    train_anno = os.path.join(input, "train", "leaf_instances")
+
+    val_images = os.path.join(input, "val", "images")
+    val_anno = os.path.join(input, "val", "leaf_instances")
+
+    callbacks = [
+        #keras.callbacks.EarlyStopping(patience=5),
+        keras.callbacks.ModelCheckpoint(filepath='checkpoints/model_##name##.{epoch:02d}_##data##.keras'.replace("##name##", model).replace('##data##', os.path.basename(input))),
+        keras.callbacks.TensorBoard(log_dir='./logs'),
+        keras.callbacks.ModelCheckpoint(filepath='out/best_##name##_##data##.keras'.replace('##name##', model).replace('##data##', os.path.basename(input)), save_best_only=True, mode='max', monitor='val_mean_io_u')
+    ]
+
+    train(model, 
+          train_images, 
+          train_anno,
+          validate=not no_validate,
+          val_images=val_images, 
+          val_annotations=val_anno, 
+          #callbacks=callbacks, 
+          do_augment=augment,
+          n_classes=classes)
+    
+if __name__ == '__main__':
+    main()
