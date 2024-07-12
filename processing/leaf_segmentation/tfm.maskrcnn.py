16e943dffc773586fb61658c88e6f58d549403e2
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-12 22:53:46 +0200

    kp

--- processing/leaf_segmentation/tfm.maskrcnn.py
+++ processing/leaf_segmentation/tfm.maskrcnn.py
@@ -29,6 +29,7 @@ config.trainer.checkpoint_interval = 1000
 config.trainer.optimizer_config.learning_rate.type = 'stepwise'
 config.trainer.optimizer_config.learning_rate.stepwise.boundaries = [6000, 8000]
 config.trainer.optimizer_config.learning_rate.stepwise.values = [0.08, 0.008, 0.0008]
+config.norm_activation = 1
 
 def preprocess_example(example):
     image = example['image']
