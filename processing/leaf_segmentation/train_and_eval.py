13705683a9641eed76d05a9f680198966eacc001
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-12 12:09:31 +0200

    mc loss

--- processing/leaf_segmentation/train_and_eval.py
+++ processing/leaf_segmentation/train_and_eval.py
@@ -56,24 +56,39 @@ def execute(model, name=None, lab=False, batch_size=32, epochs=15, data='_data',
         iou = iou_loss(y_true, y_pred)
         return bce_loss - iou
     
-    def jaccard_distance_loss(y_true, y_pred, smooth=100):
+    def multiclass_weighted_tanimoto_loss(class_weights: Union[list, np.ndarray, tf.Tensor]) -> Callable[[tf.Tensor, tf.Tensor], tf.Tensor]:
         """
-        Jaccard = (|X & Y|)/ (|X|+ |Y| - |X & Y|)
-                = sum(|A*B|)/(sum(|A|)+sum(|B|)-sum(|A*B|))
-        
-        The jaccard distance loss is usefull for unbalanced datasets. This has been
-        shifted so it converges on 0 and is smoothed to avoid exploding or disapearing
-        gradient.
-        
-        Ref: https://en.wikipedia.org/wiki/Jaccard_index
-        
-        @url: https://gist.github.com/wassname/f1452b748efcbeb4cb9b1d059dce6f96
-        @author: wassname
+        Weighted Tanimoto loss.
+
+        Defined in the paper "ResUNet-a: a deep learning framework for semantic segmentation of remotely sensed data",
+        under 3.2.4. Generalization to multiclass imbalanced problems. See https://arxiv.org/pdf/1904.00592.pdf
+
+        Used as loss function for multi-class image segmentation with one-hot encoded masks.
+
+        :param class_weights: Class weight coefficients (Union[list, np.ndarray, tf.Tensor], len=<N_CLASSES>)
+        :return: Weighted Tanimoto loss function (Callable[[tf.Tensor, tf.Tensor], tf.Tensor])
         """
-        intersection = K.sum(K.sum(K.abs(y_true * y_pred), axis=-1))
-        sum_ = K.sum(K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1))
-        jac = (intersection + smooth) / (sum_ - intersection + smooth)
-        return (1 - jac) * smooth
+        if not isinstance(class_weights, tf.Tensor):
+            class_weights = tf.constant(class_weights)
+
+        def loss(y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
+            """
+            Compute weighted Tanimoto loss.
+
+            :param y_true: True masks (tf.Tensor, shape=(<BATCH_SIZE>, <IMAGE_HEIGHT>, <IMAGE_WIDTH>, <N_CLASSES>))
+            :param y_pred: Predicted masks (tf.Tensor, shape=(<BATCH_SIZE>, <IMAGE_HEIGHT>, <IMAGE_WIDTH>, <N_CLASSES>))
+            :return: Weighted Tanimoto loss (tf.Tensor, shape=(None, ))
+            """
+            axis_to_reduce = range(1, K.ndim(y_pred))  # All axis but first (batch)
+            numerator = y_true * y_pred * class_weights
+            numerator = K.sum(numerator, axis=axis_to_reduce)
+
+            denominator = (y_true**2 + y_pred**2 - y_true * y_pred) * class_weights
+            denominator = K.sum(denominator, axis=axis_to_reduce)
+            return 1 - numerator / denominator
+
+        return loss
+
 
     model.build(keras.Input((batch_size, ) + INPUT_SHAPE))
 
@@ -82,7 +97,7 @@ def execute(model, name=None, lab=False, batch_size=32, epochs=15, data='_data',
     model.compile(
         #loss=DiceLoss(),
         #loss='categorical_crossentropy',
-        loss=jaccard_distance_loss,
+        loss=multiclass_weighted_tanimoto_loss(None),
         optimizer=opt,
         metrics=[
             keras.metrics.OneHotMeanIoU(
