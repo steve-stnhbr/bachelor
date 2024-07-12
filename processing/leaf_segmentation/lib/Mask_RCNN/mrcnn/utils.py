8142d5147fa93aa7827fca660ad0394808798282
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-12 14:02:18 +0200

    kp

--- processing/leaf_segmentation/lib/Mask_RCNN/mrcnn/utils.py
+++ processing/leaf_segmentation/lib/Mask_RCNN/mrcnn/utils.py
@@ -529,7 +529,7 @@ def minimize_mask(bbox, mask, mini_shape):
         if m.size == 0:
             raise Exception("Invalid bounding box with area of zero")
         # Resize with bilinear interpolation
-        m = resize(m, mini_shape)
+        m = resize(m, mini_shape.astype(uint8))
         mini_mask[:, :, i] = np.around(m).astype(np.bool)
     return mini_mask
 
