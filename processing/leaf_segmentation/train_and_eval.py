8f7be863f390dc45eeb7fd428fae21829eb29cf8
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-12 12:05:24 +0200

    jac

--- processing/leaf_segmentation/train_and_eval.py
+++ processing/leaf_segmentation/train_and_eval.py
@@ -54,7 +54,26 @@ def execute(model, name=None, lab=False, batch_size=32, epochs=15, data='_data',
     def combined_bce_iou_loss(y_true, y_pred):
         bce_loss = tf.keras.losses.binary_crossentropy(y_true, y_pred)
         iou = iou_loss(y_true, y_pred)
-        return bce_loss + iou
+        return bce_loss - iou
+    
+    def jaccard_distance_loss(y_true, y_pred, smooth=100):
+        """
+        Jaccard = (|X & Y|)/ (|X|+ |Y| - |X & Y|)
+                = sum(|A*B|)/(sum(|A|)+sum(|B|)-sum(|A*B|))
+        
+        The jaccard distance loss is usefull for unbalanced datasets. This has been
+        shifted so it converges on 0 and is smoothed to avoid exploding or disapearing
+        gradient.
+        
+        Ref: https://en.wikipedia.org/wiki/Jaccard_index
+        
+        @url: https://gist.github.com/wassname/f1452b748efcbeb4cb9b1d059dce6f96
+        @author: wassname
+        """
+        intersection = K.sum(K.sum(K.abs(y_true * y_pred), axis=-1))
+        sum_ = K.sum(K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1))
+        jac = (intersection + smooth) / (sum_ - intersection + smooth)
+        return (1 - jac) * smooth
 
     model.build(keras.Input((batch_size, ) + INPUT_SHAPE))
 
@@ -63,7 +82,7 @@ def execute(model, name=None, lab=False, batch_size=32, epochs=15, data='_data',
     model.compile(
         #loss=DiceLoss(),
         #loss='categorical_crossentropy',
-        loss=combined_bce_iou_loss,
+        loss=jaccard_distance_loss,
         optimizer=opt,
         metrics=[
             keras.metrics.OneHotMeanIoU(
