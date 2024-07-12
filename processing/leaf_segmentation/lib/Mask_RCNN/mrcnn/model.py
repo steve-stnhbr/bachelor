84d0eeadf148f3fa8b771b49d051e1c189ac75e6
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-12 13:36:53 +0200

    tolle

--- processing/leaf_segmentation/lib/Mask_RCNN/mrcnn/model.py
+++ processing/leaf_segmentation/lib/Mask_RCNN/mrcnn/model.py
@@ -2380,7 +2380,7 @@ class MaskRCNN():
             callbacks=callbacks,
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
-            max_queue_size=100,
+            #max_queue_size=100,
             workers=workers,
             use_multiprocessing=True,
         )
