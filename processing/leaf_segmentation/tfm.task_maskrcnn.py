d56b1aaed61d3da9f55065ea0fde7e92c29260e8
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-13 15:24:02 +0200

    set decoder

--- processing/leaf_segmentation/tfm.task_maskrcnn.py
+++ processing/leaf_segmentation/tfm.task_maskrcnn.py
@@ -50,17 +50,9 @@ def build_experiment_config():
     exp_config.trainer.optimizer_config.learning_rate.cosine.initial_learning_rate = 0.07
     exp_config.trainer.optimizer_config.warmup.linear.warmup_learning_rate = 0.05
 
-    class CustomTfExampleDecoder(TfExampleDecoder):
-        def __init__(self):
-            super().__init__()
-
-        def decode(self, serialized_example):
-            print(serialized_example)
-            return {}
-
     # In your build_experiment_config function:
-    exp_config.task.train_data.decoder = CustomTfExampleDecoder()
-    exp_config.task.validation_data.decoder = CustomTfExampleDecoder()
+    exp_config.task.train_data.decoder = TfExampleDecoder()
+    exp_config.task.validation_data.decoder =   TfExampleDecoder()
 
     return exp_config
 
