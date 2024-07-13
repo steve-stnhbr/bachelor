f72f9b8898f7363d2cd506d89e808287b116af72
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-13 08:55:26 +0200

    ll

--- processing/leaf_segmentation/tfm.maskrcnn.py
+++ processing/leaf_segmentation/tfm.maskrcnn.py
@@ -21,21 +21,21 @@ import numpy as np
 config = MaskRCNNConfig()
 
 # Modify the configuration as needed
-config.task.model.num_classes = 91  # Number of COCO classes
-config.task.model.mask_head.num_convs = 4
-config.task.model.mask_head.num_filters = 256
-config.task.model.roi_sampler.num_samples = 512
-config.task.losses.l2_weight_decay = 0.00004
-config.task.train_data.global_batch_size = 16
-config.task.validation_data.global_batch_size = 16
-config.trainer.train_steps = 10000
-config.trainer.validation_steps = 100
-config.trainer.validation_interval = 1000
-config.trainer.checkpoint_interval = 1000
-config.trainer.optimizer_config.learning_rate.type = 'stepwise'
-config.trainer.optimizer_config.learning_rate.stepwise.boundaries = [6000, 8000]
-config.trainer.optimizer_config.learning_rate.stepwise.values = [0.08, 0.008, 0.0008]
-config.norm_activation = 1
+# config.task.model.num_classes = 91  # Number of COCO classes
+# config.task.model.mask_head.num_convs = 4
+# config.task.model.mask_head.num_filters = 256
+# config.task.model.roi_sampler.num_samples = 512
+# config.task.losses.l2_weight_decay = 0.00004
+# config.task.train_data.global_batch_size = 16
+# config.task.validation_data.global_batch_size = 16
+# config.trainer.train_steps = 10000
+# config.trainer.validation_steps = 100
+# config.trainer.validation_interval = 1000
+# config.trainer.checkpoint_interval = 1000
+# config.trainer.optimizer_config.learning_rate.type = 'stepwise'
+# config.trainer.optimizer_config.learning_rate.stepwise.boundaries = [6000, 8000]
+# config.trainer.optimizer_config.learning_rate.stepwise.values = [0.08, 0.008, 0.0008]
+# config.norm_activation = 1
 
 def preprocess_example(example):
     image = example['image']
