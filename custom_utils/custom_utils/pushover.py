be88a23e195382464ebb26c53d2d4deea640fb01
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-14 14:12:27 +0200

    priority

--- custom_utils/custom_utils/pushover.py
+++ custom_utils/custom_utils/pushover.py
@@ -3,7 +3,7 @@ import click
 import http.client, urllib
 import requests
 
-def send_pushover_notification(message, title=None, file=None, file_format=None, verbose=False):
+def send_pushover_notification(message, title=None, priority=0, file=None, file_format=None, verbose=False):
     USER_KEY = os.getenv("PUSHOVER_USER_KEY")
     API_TOKEN = os.getenv("PUSHOVER_API_TOKEN")
     if USER_KEY is None:
@@ -18,7 +18,8 @@ def send_pushover_notification(message, title=None, file=None, file_format=None,
             "token": API_TOKEN,
             "user": USER_KEY,
             "message": message,
-            "title": title
+            "title": title,
+            "priority": priority,
         }
     files = None
     if file is not None:
