198ec8f662a0ca4473a0a83fb02b3df4fabaef0a
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-14 12:47:45 +0200

    error handling

--- custom_utils/custom_utils/pushover.py
+++ custom_utils/custom_utils/pushover.py
@@ -2,14 +2,22 @@ import os
 import click
 import http.client, urllib
 
-USER_KEY = os.environ["PUSHOVER_USER_KEY"]
-API_TOKEN = os.environ["PUSHOVER_API_TOKEN"]
 
 def send_pushover_notification(message, title=None):
+    if "PUSHOVER_USER_KEY" not in os.environ:
+        print("No user key provided, aborting pushover notification")
+        print("Consider adding 'PUSHOVER_USER_KEY' to your environment variables")
+        return
+    if "PUSHOVER_API_TOKEN" not in os.environ:
+        print("No user key provided, aborting pushover notification")
+        print("Consider adding 'PUSHOVER_API_TOKEN' to your environment variables")
+        return
+    USER_KEY = os.environ["PUSHOVER_USER_KEY"]
+    API_TOKEN = os.environ["PUSHOVER_API_TOKEN"]
     conn = http.client.HTTPSConnection("api.pushover.net:443")
     conn.request("POST", "/1/messages.json",
     urllib.parse.urlencode({
-        "token": APP_TOKEN,
+        "token": API_TOKEN,
         "user": USER_KEY,
         "message": message,
         "title": title
