9cdae685bb2da81a17d7aaae3402677119ef7af5
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-14 13:20:37 +0200

    bruv

--- custom_utils/custom_utils/pushover.py
+++ custom_utils/custom_utils/pushover.py
@@ -3,7 +3,7 @@ import click
 import http.client, urllib
 import requests
 
-def send_pushover_notification(message, title=None):
+def send_pushover_notification(message, title=None, file=None, file_format=None):
     USER_KEY = os.getenv("PUSHOVER_USER_KEY")
     API_TOKEN = os.getenv("PUSHOVER_API_TOKEN")
     if "PUSHOVER_USER_KEY" == None:
@@ -14,16 +14,18 @@ def send_pushover_notification(message, title=None):
         print("No user key provided, aborting pushover notification")
         print("Consider adding 'PUSHOVER_API_TOKEN' to your environment variables")
         return
-    r = requests.post("https://api.pushover.net/1/messages.json", data = {
-            "token": "APP_TOKEN",
-            "user": "USER_KEY",
-            "message": "hello world"
-        },
+    data = {
+            "token": API_TOKEN,
+            "user": USER_KEY,
+            "message": message,
+            "title": title
+        }
+    files = None
+    if file is not None:
         files = {
-            "attachment": ("image.jpg", open("your_image.jpg", "rb"), "image/jpeg")
-        },
-        timeout=800
-    )
+            "attachment": (file, open(file, "rb"), file_format)
+        }
+    r = requests.post("https://api.pushover.net/1/messages.json", data=data, files=files,timeout=800)
     print("Sent notification:", title)
     return r
 
