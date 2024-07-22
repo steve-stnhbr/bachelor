ab5c8724a9ee10ae0b7f787173f0786357c98301
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-07-22 10:03:44 +0200

    bibi

---
+++ writing/plot_tfevents.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from tensorflow.python.summary.summary_iterator import summary_iterator
+import click
+
+@click.command()
+@click.argument("input_file")
+def main(input_file):
+    if "," in input_file:
+        print("Multiple files not supported yet!")
+        input_files = input_file.split(",")
+
+    for summary in (input_file):
+        print(summary)
