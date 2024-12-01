25821b0bd0682d21618ad8f562def4a27bb17e5c
Stefan Steinheber <e12022506@student.tuwien.ac.at>
2024-12-01 11:37:39 +0100

    spassig

--- processing/leaf_segmentation/one_class_detection.py
+++ processing/leaf_segmentation/one_class_detection.py
@@ -33,7 +33,7 @@ class SingleClassDataset(Dataset):
 folder_path = '_data/urban_street0_25/images'
 
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
+    transforms.Resize((320, 320)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
